710919c23662b83833bacfb7deea78c17f489b2f
DANJO048 <42050651+DANJO048@users.noreply.github.com>
2018-09-02 21:18:53 -0600

    Update Hola_Mundo.java

--- Hola_Mundo.java
+++ Hola_Mundo.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*Programa Hola Mundo*/
 package hola_mundo;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Josué Vásquez
- */
-public class Hola_Mundo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+/**@author Josué Vásquez**/
+public class Hola_Mundo
+{
+    public static void main(String[] args)
+    {
         JOptionPane.showMessageDialog(null, "HOLA MUNDO");//Opción ventana emergente
         System.out.println("HOLA MUNDO");//Opción mensaje en Consola
     }
